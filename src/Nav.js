c5613514bc4e19b91740a448351beb40db3b4aa2
DevAash <pgmeraash@gmail.com>
2020-07-19 10:48:57 +0530

    Initial Commit

---
+++ src/Nav.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import './Nav.css'
+function Nav() {
+    const [show,handleShow] = useState(false);
+    // when u scroll down u need to do something
+    useEffect(()=>{
+        window.addEventListener("scroll",() =>{
+        if(window.scrollY>100){
+            // handling scroll run this fuction
+            handleShow(true);
+        }
+        else{
+            handleShow(false);
+        }
+    });
+        return () =>{
+            window.removeEventListener("scroll");
+        };
+    },[]);
+
+    return (
+        <div className={`nav ${show && "nav__black"}`}>
+            <img class="nav__logo" src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png" alt="Netflix Logo"></img>
+            <img class="nav__avatar" src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="Netflix Logo"></img>
+        </div>
+    )
+}
+
+export default Nav
---
+++ src/request.js
@@ -0,0 +1,13 @@
+const API_KEY = "045d181a05d1ae54ad0da5b2d4e982d5"; 
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
